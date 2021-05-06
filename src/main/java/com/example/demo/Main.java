985494e1507ca4c1fd04fe446b4d3e6e87fd0a4f
hyechan <dhgpcks123@naver.com>
2021-05-06 13:43:54 +0900

    어댑터(Adapter) 패턴

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.adapter.*;
 import com.example.demo.singleton.AClazz;
 import com.example.demo.singleton.BClazz;
 import com.example.demo.singleton.SocketClient;
 
 public class Main {
     public static void main(String[] args) {
-
+/*
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -14,5 +15,24 @@ public class Main {
         SocketClient bClient = bClazz.getSocketClient();
 
         System.out.println("두개의 객체가 동일한가? : " + aClient.equals(bClient));
+ */
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+//        connect(cleaner); // 220V -> 110V로 바꿔줘야 해. 어댑터를 만들어보자!
+
+        //어댑터를 하나 사옵니다.
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
+    }
+
+    // 콘센트
+    public static void connect(Electronic110V electronic110V){
+        electronic110V.powerOn();
     }
 }
