b19e1c1bcd47296d769437e8bfbd448bd8a1cedb
hyechan <dhgpcks123@naver.com>
2021-05-06 14:13:05 +0900

    proxy패턴을 활용한 cache,AOP

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,34 +1,51 @@
 package com.example.demo;
 
 import com.example.demo.adapter.*;
+import com.example.demo.aop.AopBrowser;
+import com.example.demo.proxy.Browser;
+import com.example.demo.proxy.IBrowser;
 import com.example.demo.singleton.AClazz;
 import com.example.demo.singleton.BClazz;
 import com.example.demo.singleton.SocketClient;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
     public static void main(String[] args) {
-/*
-        AClazz aClazz = new AClazz();
-        BClazz bClazz = new BClazz();
 
-        SocketClient aClient = aClazz.getSocketClient();
-        SocketClient bClient = bClazz.getSocketClient();
+        /*
+        Browser browser = new Browser("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        //BrowserProxy를 활용한 cache기능!
+        IBrowser browser = new Browser("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        */
 
-        System.out.println("두개의 객체가 동일한가? : " + aClient.equals(bClient));
- */
-        HairDryer hairDryer = new HairDryer();
-        connect(hairDryer);
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
 
-        Cleaner cleaner = new Cleaner();
-//        connect(cleaner); // 220V -> 110V로 바꿔줘야 해. 어댑터를 만들어보자!
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+        );
 
-        //어댑터를 하나 사옵니다.
-        Electronic110V adapter = new SocketAdapter(cleaner);
-        connect(adapter);
-
-        AirConditioner airConditioner = new AirConditioner();
-        Electronic110V airAdapter = new SocketAdapter(airConditioner);
-        connect(airAdapter);
+        aopBrowser.show();
+        System.out.println("loding time : " + end.get());
+        aopBrowser.show();
+        System.out.println("loding time : " + end.get());
     }
 
     // 콘센트
